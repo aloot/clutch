1953a9c55d4864a0dfa197cb9a9ab4e850abf1b3
Samuel Karlsson <Samuel Karlsson>
2017-02-20 14:38:14 +0100

    amuel ändrar lite till

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
   public static void main (String[] args) {
     System.out.println("Java Anna was here Java");
-    System.out.println("Git sucks");    
+    System.out.println("Git sucks, or does it.......?");    
     System.out.println("And so was Samuel!");
 
   }
