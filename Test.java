bda8c73549b46dca67458d82b15a633069b28dac
Samuel Karlsson <Samuel Karlsson>
2017-02-20 14:25:49 +0100

    samuel ändrar lite

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
   public static void main (String[] args) {
-    System.out.println("Java Anna was here Java")i
-	   
+    System.out.println("Java Anna was here Java");
+    System.out.println("And so was Samuel!");
+
   }
 }
-
