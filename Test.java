a77ab8b40fb28a63f1c6a36f57c939c3ce650062
Anna Kry <Anna Kry>
2017-02-20 14:44:13 +0100

    testar konflikter

--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 public class Test {
   public static void main (String[] args) {
     System.out.println("Java Anna was here Java");
-    System.out.println("Git sucks");    
+    System.out.println("Git sucks");
+    Anna igen
     System.out.println("And so was Samuel!");
 
   }
